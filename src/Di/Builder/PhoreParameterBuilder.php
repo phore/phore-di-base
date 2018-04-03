f61a9a6a19237051c6bde3cbd89f68c0b61f7fdc
matthes <leuffen@continue.de>
2018-04-03 14:23:51 +0200

    Added PSR-11

--- src/Di/Builder/PhoreParameterBuilder.php
+++ src/Di/Builder/PhoreParameterBuilder.php
@@ -14,25 +14,8 @@
     {
 
 
-
-
-        public function buildParamDef (callable $callable) : PhoreCallbackParameterDef {
+        private function buildFromParameterArray (array $parameterRef) : PhoreCallbackParameterDef {
             $paramDef = new PhoreCallbackParameterDef();
-
-            if (is_array($callable)) {
-                if (is_object($callable[0])) {
-                    $ref = new \ReflectionMethod(get_class($callable[0]), $callable[1]);
-                } else if (is_string($callable[0])) {
-                    $ref = new \ReflectionMethod($callable[0], $callable[1]);
-                } else {
-                    throw new \InvalidArgumentException("Array is no valid callback." . var_export($callable, true));
-                }
-            } else {
-                $ref = new \ReflectionFunction($callable);
-            }
-
-            $parameterRef = $ref->getParameters();
-
             for ($paramIndex = 0; $paramIndex<count ($parameterRef); $paramIndex++) {
                 $curParam = $parameterRef[$paramIndex];
 
@@ -58,6 +41,32 @@
                 ];
             }
             return $paramDef;
+        }
+
+
+        public function buildParamDefForConstructor ($class) : PhoreCallbackParameterDef
+        {
+            $ref = new \ReflectionClass($class);
+            if (($constructorRef = $ref->getConstructor()) === null) {
+                return new PhoreCallbackParameterDef();
+            }
+            return $this->buildFromParameterArray($constructorRef->getParameters());
+        }
+
+
+        public function buildParamDef (callable $callable) : PhoreCallbackParameterDef {
+            if (is_array($callable)) {
+                if (is_object($callable[0])) {
+                    $ref = new \ReflectionMethod(get_class($callable[0]), $callable[1]);
+                } else if (is_string($callable[0])) {
+                    $ref = new \ReflectionMethod($callable[0], $callable[1]);
+                } else {
+                    throw new \InvalidArgumentException("Array is no valid callback." . var_export($callable, true));
+                }
+            } else {
+                $ref = new \ReflectionFunction($callable);
+            }
+            return $this->buildFromParameterArray($ref->getParameters());
         }
 
         public function buildParams (PhoreCallbackParameterDef $paramDef, PhoreParameterBuilderCallback $builder, array $values)
