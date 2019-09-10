99f02c608f92070eb65a7c140462bf6b32fdfd57
Michael <michael.schaar@talpa-solutions.com>
2019-09-10 14:26:27 +0200

    functions.php and tests

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,35 @@
 <?php
 
+use Phore\Di\Builder\PhoreCallbackParameterDef;
 
-function phore_func_params() : array {
-
+/**
+ * @param callable $callable
+ * @return ReflectionParameter[]
+ */
+function phore_func_params($callable) : array {
+    if (is_array($callable)) {
+        if (is_object($callable[0])) {
+            if($callable[1] === "__construct") {
+                $ref = new \ReflectionClass(get_class($callable[0]));
+                if (($constructorRef = $ref->getConstructor()) === null) {
+                    return [];
+                } else {
+                    return $ref->getConstructor()->getParameters();
+                }
+            } elseif (is_callable($callable)) {
+                $ref = new \ReflectionMethod(get_class($callable[0]), $callable[1]);
+                return $ref->getParameters();
+            } else {
+                throw new \InvalidArgumentException("Array is not callable.");
+            }
+        } else if (is_string($callable[0])) {
+            $ref = new \ReflectionMethod($callable[0], $callable[1]);
+            return $ref->getParameters();
+        } else {
+            throw new \InvalidArgumentException("Array is no valid callback.");
+        }
+    } else {
+        $ref = new \ReflectionFunction($callable);
+        return $ref->getParameters();
+    }
 }
--- test/Di/PhoreBaseDiCallerTest.php
+++ test/Di/PhoreBaseDiCallerTest.php
@@ -9,9 +9,10 @@
     namespace Phore\Test\Di;
 
 
-    use Phore\Di\PhoreBaseDiCaller;
+    use Phore\Di\Caller\PhoreBaseDiCaller;
+    use PHPUnit\Framework\TestCase;
 
-    class PhoreBaseDiCallerTest extends \PHPUnit_Framework_TestCase
+    class PhoreBaseDiCallerTest extends TestCase
     {
 
 
