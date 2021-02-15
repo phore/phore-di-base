e2463be4dfd638a41455893df7a3e16ca8d0dc64
Matthes <matthes@leuffen.de>
2021-02-15 10:48:09 +0100

    update

--- src/Di/Container/DiContainer.php
+++ src/Di/Container/DiContainer.php
@@ -148,4 +148,20 @@ class DiContainer implements ContainerInterface
         return $this;
     }
 
+
+
+    public function __get($name)
+    {
+        return $this->resolve($name);
+    }
+
+    public function __set($name, $val)
+    {
+        throw new \InvalidArgumentException("Trying to set '$name' on app. Use define() to inject something.");
+    }
+
+    public function __isset ($name)
+    {
+        return $this->has($name);
+    }
 }
